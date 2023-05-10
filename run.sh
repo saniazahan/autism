25ce258cf46584e2ee13cc20626322a21b335931
Sania Zahan <sania.cse@gmail.com>
2023-05-10 12:46:48 +0800

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,8 @@
+# Train
+python -u main.py --config train.yaml --phase train --work-dir ./exps/vid_baseline_random
+
+
+
+# Test
+#python -u main.py --config test.yaml --work-dir ./exps/cross2/2/test
+#python -u main_2.py --config test.yaml --work-dir ./exps/EDW_random/t #1/test
